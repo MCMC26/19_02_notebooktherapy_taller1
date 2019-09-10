289e461674b6b7a0fdd02b4899852d38e0a4d28c
Camila Marmolejo <camilamarmol26@gmail.com>
2019-09-10 07:56:18 -0500

    Hola profe

--- script/script.js
+++ script/script.js
@@ -1,9 +1,13 @@
 function handleLoad() {
     function handleScroll(event){
         var nav = document.querySelector('.mainheader');
+        var navItem = document.querySelectorAll('.mainnav__item');
         var height = nav.offsetHeight;
         if(window.scrollY > height){
             nav.classList.add('mainheader--scrolled');
+            for (let i = 0; i < navItem.length; i++) {
+                navItem[i].classList.add('mainnav__item--scrolled');                
+            }
         } else {
             console.log('mainheader--scrolled')
             nav.classList.remove>('mainheader--scrolled');
@@ -11,5 +15,15 @@ function handleLoad() {
         
     }
     window.addEventListener('scroll', handleScroll);
-}
+
+    }
 window.addEventListener('load', handleLoad);
+
+var btn = document.querySelector('.mainheader__btn');
+var navi = document.querySelector('.mainheader__mainnav');
+
+function handleClick(){
+    navi.classList.toggle('mainheader__mainnav--active');
+}
+
+btn.addEventListener('click', handleClick);
